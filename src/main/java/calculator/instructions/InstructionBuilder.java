6c8a754b6d170a6deeb72c7be9562687441c0e10
maxdiland <maxdiland@gmail.com>
2018-05-24 23:25:09 +0300

    Update InstructionBuilder.java

--- src/main/java/calculator/instructions/InstructionBuilder.java
+++ src/main/java/calculator/instructions/InstructionBuilder.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InstructionBuilder {
-    private List<InstructionInt> instructionInts = new ArrayList<>();
+    private List<InstructionInt> instructionInts = new ArrayList<>(); //Тут список инструкций. Получаеются всякие мат. действия сюда с числами сюда складываются. смотри где происходит instructionInts.add()
 
     public static List<InstructionInt> readFromStream(InputStream inputStream) throws IOException {
         Reader reader = new InputStreamReader(inputStream);
@@ -17,13 +17,13 @@ public class InstructionBuilder {
         BufferedReader bufferedReader = new BufferedReader(reader);
 
         String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            String[] split = line.split(" ");
-            Double value = Double.valueOf(split[1]);
-            builder.addInstruction(split[0], value);
+        while ((line = bufferedReader.readLine()) != null) { //bufferedReader в отличии от других Reader-ов умеет читать построчно. Потму да, в переменную line записывается прочетнная строка. Как только дойдем до конца файла - прочтется null и следовательно следующей итерации while не будет
+            String[] split = line.split(" "); // Не совсем. split бьет строку на кусочки. В данном случае разбивка происходит по пробелу " "
+            Double value = Double.valueOf(split[1]); // и берется второй по счету (по индексу 1) кусочек из прочтенной строки и парсится в дабл
+            builder.addInstruction(split[0], value); // а тут берется первый по счету (нулевой по индексу) кусочек из строки. Видимо строка является инструкцией, где 1 кусочек - действие, 2 кусочек - число
 
         }
-        return builder.build();
+        return builder.build(); // Типичный шаблон проектирования builder. Когда ты для того чтобы построить готовый объект вызываешь промежуточные методы, а когда закончил наполнение данными, вызвал build и на основании введеных даных сконструируется объект
     }
 
     public InstructionBuilder addInstruction(String instruction, Double value) {
@@ -67,7 +67,7 @@ public class InstructionBuilder {
         return this;
     }
 
-    public List<InstructionInt> build() {
+    public List<InstructionInt> build() { // Видимо автор решил использовать шаблон проектирования builder
         return instructionInts;
     }
 }
