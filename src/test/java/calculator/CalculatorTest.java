64e3fef7a62b8d00cfd79ab9fbb1ac8f7eea2c37
maxdiland <maxdiland@gmail.com>
2018-05-24 23:50:07 +0300

    Update CalculatorTest.java

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -15,7 +15,7 @@ public class CalculatorTest {
     public void calculateTest() throws Exception {
         Calculator calculator = new Calculator();
 
-        List<InstructionInt> instructionInts = new InstructionBuilder().subtract(3d).apply(10d).build();
+        List<InstructionInt> instructionInts = new InstructionBuilder().subtract(3d).apply(10d).build(); // Так принято, что с листами и сетами и мапами обычно работают на уровне интерфесов. Удобство в том, что сегодня используется ArrayList, а завтра нужно будет перейти на LinkedList. В связи с тем, что оба этих класса являются List-ами, твой код продолжит компилироваться и работать и после подмены реализации. Собственно это и есть пример полиморфизма.
 
         assertThat(calculator.calculate(instructionInts), is(7d));
     }
